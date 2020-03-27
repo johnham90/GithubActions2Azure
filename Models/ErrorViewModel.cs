eb334ec40aa98fc86e82ee5d18945e0cc2981da6
John Ham <hamj1990@gmail.com>
2020-03-27 11:00:42 -0700

    1st commmit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GitHubActions2Azure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
